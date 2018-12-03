d60ca7c9c6530cee61d8fcf7cc6a9d9db25a3536
guledali <guled.ali01@gmail.com>
2018-12-03 15:32:36 +0100

    Small update!

--- Frontend/cypress/integration/Form.spec.js
+++ Frontend/cypress/integration/Form.spec.js
@@ -1,11 +1,15 @@
 describe('Visit site', () => {
   it('run site!!', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('#inlineFormInput').type('johnny')
-    cy.get('#submitButton').click({ force: true })
-    cy.wait(500);
+    cy.get('[data-testid=inputForm]').type('johnny')
+    cy.get('#submitButton').click()
+    cy.wait(1000);
     cy.get('#inlineFormInput').type('Lena')
-    cy.get('#submitButton').click({ force: true })
+    cy.get('#submitButton').click()
+    cy.wait(1000);
+    cy.get('#deleteTask').click({ force: true })
+    cy.wait(1000);
+    cy.get('#deleteTask').click({ force: true })
     cy.wait(1000);
     cy.get('#deleteTask').click({ force: true })
     cy.wait(1000);
