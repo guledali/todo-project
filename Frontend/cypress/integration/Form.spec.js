a7fba82e8f34009f0598da87eb1b17a83deb3f9f
guledali <guled.ali01@gmail.com>
2018-12-03 14:56:52 +0100

    Added { force: true } on cypress testing!

--- Frontend/cypress/integration/Form.spec.js
+++ Frontend/cypress/integration/Form.spec.js
@@ -2,13 +2,13 @@ describe('Visit site', () => {
   it('run site!!', () => {
     cy.visit('http://localhost:3000/');
     cy.get('#inlineFormInput').type('johnny')
-    cy.get('#submitButton').click()
+    cy.get('#submitButton').click({ force: true })
     cy.wait(500);
     cy.get('#inlineFormInput').type('Lena')
-    cy.get('#submitButton').click()
+    cy.get('#submitButton').click({ force: true })
     cy.wait(1000);
-    cy.get('#deleteTask').click()
+    cy.get('#deleteTask').click({ force: true })
     cy.wait(1000);
-    cy.get('#deleteTask').click()
+    cy.get('#deleteTask').click({ force: true })
   })
 })
